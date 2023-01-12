16c27d54c99e94ca1c2f64cebbd9ecb7588c45dd
SeanChinJunKai <seanchinjk1@gmail.com>
2023-01-12 10:10:04 +0800

    Converted lint to async

--- find.ts
+++ find.ts
@@ -1,8 +1,9 @@
 import * as fs from 'fs';
-import * as babel_parser from '@babel/parser';
 import * as t from "@babel/types";
+import * as babel_parser from '@babel/parser';
 import traverse from '@babel/traverse';
-import * as prettier from "prettier"
+import * as prettier from "prettier";
+
 
 const filePath = "./sample.ts";
 
@@ -10,29 +11,43 @@ const filePath = "./sample.ts";
 //  reads the file located at filePath and returns its contents as a string
 const fileContent = fs.readFileSync(filePath, 'utf8');
 
-// parses the string from filecontent into an Abstract Syntax Tree
+// parses the string from filecontent into an Abstract Syntax Tree (AST)
 const ast = babel_parser.parse(fileContent, {
     sourceType: 'module',
     plugins: ['typescript']
 });
 
- function lint(code: string) {
+async function lint(code: string) {
     return prettier.format(code, {"parser" : "babel"});
+
 }
 
+let literals: string[] = []
 
+// Traversing the AST
 traverse(ast, {
-    enter(path) {
+    enter(path){
         const node = path.node;
         if (t.isTemplateLiteral(node)) {
             if (node.leadingComments) {
                 const tsxComments = node.leadingComments.filter(comment => comment.value === "tsx");
                 if (tsxComments.length > 0) {
-                    const linted = lint(node.quasis[0].value.raw)
-                    console.log(linted)
+                    literals.push(node.quasis[0].value.raw)
                 }
             }
         }
+
     }
 });
+
+async function lintPrintting() {
+    for (let i = 0; i < literals.length; i++) {
+        const linted = await lint(literals[i])
+        console.log(linted)
+    }
+}
+
+lintPrintting();
+
+
 
